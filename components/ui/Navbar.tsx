85cb1f4a1ceaff3082e00a4df25c75180fe3c0d7
AudryLopez <alopez@intellisys.com.do>
2022-03-28 20:52:14 -0400

    nav link to home

--- components/ui/Navbar.tsx
+++ components/ui/Navbar.tsx
@@ -1,9 +1,12 @@
 import { useTheme } from "@nextui-org/react";
 import { Text } from "@nextui-org/react";
 import Image from "next/image";
+import Link from "next/link";
+import NextLink from "next/link";
 
 function Navbar() {
     const { theme } = useTheme()
+
     return (
       <div
         style={{
@@ -16,15 +19,17 @@ function Navbar() {
           backgroundColor: theme?.colors.gray900.value,
         }}
       >
-        <Image 
-            src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/25.svg"
-            alt="Pokemon"
-            width={70}
-            height={50}
+        <Image
+          src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/25.svg"
+          alt="Pokemon"
+          width={70}
+          height={50}
         />
-        <Text color="white" h3>
-          Pokemon
-        </Text>
+        <NextLink href="/" passHref>
+            <Text color="white" h3>
+              Pokemon
+            </Text>
+        </NextLink>
       </div>
     );
 }
